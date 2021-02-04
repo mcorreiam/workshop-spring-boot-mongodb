ee9d9dbe1edf2242eb0c27ba79bea327a759f49e
Marco Correia <m.correia.m@gmail.com>
2021-02-04 18:02:24 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/marcocorreia/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.marcocorreia.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcocorreia.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
