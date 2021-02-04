dead6bc09b05c515e3d5ce20a4084b66c6b2ce5d
Marco Correia <m.correia.m@gmail.com>
2021-02-04 15:51:41 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/marcocorreia/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcocorreia.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
