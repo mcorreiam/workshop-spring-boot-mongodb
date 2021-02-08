83930390f044a53eb3091b017b0ec98d4e016a4f
Marco Correia <m.correia.m@gmail.com>
2021-02-08 14:51:49 -0300

    Obtendo um post por id

---
+++ src/main/java/com/marcocorreia/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.marcocorreia.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcocorreia.workshopmongo.domain.Post;
+import com.marcocorreia.workshopmongo.repository.PostRepository;
+import com.marcocorreia.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
